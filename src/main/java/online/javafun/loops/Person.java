c047991d2d1f0d7a2fef07c42702147808c4b32a
User <user@its.me>
2023-01-25 12:59:32 +0100

    Initial commit

---
+++ src/main/java/online/javafun/loops/Person.java
@@ -0,0 +1,2 @@
+package online.javafun.loops;public class Person {
+}
---
+++ src/test/java/online/javafun/loops/LoopsApplicationTests.java
@@ -0,0 +1,13 @@
+package online.javafun.loops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoopsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
