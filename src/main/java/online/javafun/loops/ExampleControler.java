eb9a8f29ebe5e72a7d6876aea994cd7837088f67
User <user@its.me>
2023-01-25 14:26:52 +0100

    each loop for thymeleaf, and number.sequence object

--- src/main/java/online/javafun/loops/ExampleControler.java
+++ src/main/java/online/javafun/loops/ExampleControler.java
@@ -4,11 +4,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class ExampleControler {
 
     @GetMapping("/")
     String example(Model model) {
+        List<Person> people = List.of(
+                new Person("Jan", "Nowak"),
+                new Person("Kamil", "Kowalski"),
+                new Person("Alicja", "Dąbrowska"),
+                new Person("Marta", "Cybulska")
+        );
+        model.addAttribute("people", people);
+        model.addAttribute("size", 5);
         return "example";
     }
 }
