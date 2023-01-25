53980d1d25909ee5995f6a6824a33d589d71a855
User <user@its.me>
2023-01-25 13:00:58 +0100

    add initial files

--- src/main/java/online/javafun/loops/ExampleControler.java
+++ src/main/java/online/javafun/loops/ExampleControler.java
@@ -1,2 +1,14 @@
-package online.javafun.loops;public class ExampleControler {
+package online.javafun.loops;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ExampleControler {
+
+    @GetMapping("/")
+    String example(Model model) {
+        return "example";
+    }
 }
